148dbf2cbde16cda0bc6c869a92ec7dc1164f5a7
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-10-05 23:45:54 +0200

    dummy ticket + styling

--- tickets/views.py
+++ tickets/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
+from django.http import Http404
 # Create your views here.
 
+mTickets = [2, 147, 483, 647]
 
-def tickets(req, mTicketID):
-    return render(req, "tickets/ticket.html", {"mTicketID": mTicketID})
+
+def tickets(req, uTicketID):
+    try:
+        if (uTicketID in mTickets):
+            mTicketID = uTicketID
+        return render(req, "tickets/ticket.html", {"mTicketID": mTicketID})
+    except:
+        raise Http404()
