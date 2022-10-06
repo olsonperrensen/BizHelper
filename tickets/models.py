deeb6737020ffd9d2d723667ea9de21e2f7b7700
Olson Perrensen <htcevo4gnougat@gmail.com>
2022-10-06 10:40:32 +0200

    blueprint Ticket model added

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Ticket(models.Model):
+    mID = models.PositiveIntegerField(),
+    mType = models.CharField(max_length=8),
+    mCreatedBy = models.CharField(max_length=80),
+    mDateOfCreation = models.DateField(),
+    mDaysPassed = models.PositiveSmallIntegerField(),
+    mSolved = models.BooleanField(),
+    mComment = models.CharField(max_length=500)
